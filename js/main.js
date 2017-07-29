e55e729b182f99586c74d0150baa4bc5a6696bfa
Pavlicheck <paanliff@gmail.com>
2017-07-29 16:39:18 +0300

    Cocroach3

--- js/main.js
+++ js/main.js
@@ -1,56 +1,28 @@
-var screenWidth = $(window).width(),
-	screenHeight = $(window).height(),
-	menuWidth = 0,
-	menuHeight = 0,
-	ptMenu = 0,
-	plMenu = 0
-	fzlink = 0;
-function calcSize(sW, sH) {
+// Values
+var winWidth=0,
+	winHeight=0,
+	winHalf = 0;
 
-	if (sW > sH) {
-		menuWidth = sW * .6;
-		menuHeight = sH * .7;
-		ptMenu = sH * .15;
-		plMenu = sW * .2;
- 	} else {
- 		menuWidth = sW * .9;
-		menuHeight = sH * .8;
-		ptMenu = sH * .1;
-		plMenu = sW * .05;
- 	}
- 	fzlink = parseInt(menuHeight * .2 *.4);
- 	fzcock = parseInt(menuWidth * .2 * .85);
-}
-
-calcSize(screenWidth, screenHeight);
-$('.main-wrap').height(screenHeight);
-$('.main-menu').height(menuHeight);
-$('.main-menu').width(menuWidth);
-$('.main-menu').css({"left": plMenu, 'top': ptMenu});
-$('.link span').css({'font-size': fzlink + "px"});
-$("span.cock").css({'font-size': fzcock + "px"});
-
+//resize
+	function resizeWin() {
+		winWidth = $(document).width();
+		winHeight = $(document).height();
+		if (winWidth > winHeight) {
+		$('.background').width(winWidth).height(winHeight);
+		$('.shatter-main').width(winWidth).height(winHeight);
+		$('.cockroach').width(winWidth*.15).height(winWidth*.15);
+	} else {
+		$('.background').width(winWidth).height(winHeight);
+		$('.shatter-main').width(winWidth).height(winHeight);
+		$('.cockroach').width(winWidth*.4).height(winWidth*.4);
+	}
+		// alert('works');
+		// winHalf = winWidth/2;
+	}
 
 $(window).resize(function() {
-screenWidth = $(window).width();
-screenHeight = $(window).height();
-calcSize(screenWidth, screenHeight);
-$('.main-wrap').height(screenHeight);
-$('.main-menu').height(menuHeight);
-$('.main-menu').width(menuWidth);
-$('.main-menu').css({"left": plMenu, 'top': ptMenu});
-$("span.cock").css({'font-size': fzcock + "px"});
-$('.link span').css({'font-size': fzlink + "px"});
+		resizeWin();
 });
 
-
-
-
-
-$('.link').on('mousedown', function() {
-		$('.activ').removeClass('activ');
-		$('.activ-tab').removeClass('activ-tab');
-		$($(this).attr('data-tab-class')).addClass('activ-tab');
-		$(this).addClass('activ');
-	});
-
+// Menu creater
+resizeWin();
