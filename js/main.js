8179af401c09f13d586a884b8a2e67bc4e97271c
Pavlicheck <paanliff@gmail.com>
2017-10-15 00:13:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@
 darkOpacity(0, 'none')
 
 
-function darkOpacity(opacity, display) {
+function darkOpacity(opacity, display, time) {
 $('.dark').css('display', 'block');
-$('.dark').animate({opacity: opacity}, 1000, function(){$(this).css('display', display)});
+$('.dark').animate({opacity: opacity}, time, function(){$(this).css('display', display)});
 }
 
 
 
 function scoreShow() {
-		darkOpacity(0.8, 'block');
-		$('.score').css('display','block').animate({left: 0}, 1000);
+		darkOpacity(0.8, 'block', 500);
+		$('.score').css('display','block').animate({left: 0}, 500);
+}
+
+function scoreHide() {
+		darkOpacity(0, 'none', 500);
+		$('.score').animate({left: -3000}, 500, function() {$(this).css('display','none')});
 }
 
 $('#score').click(scoreShow);
+$('.score-btn-back').on('click', scoreHide);
