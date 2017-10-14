ea067c677f9bd5b2b91ffeb9227b9f62cb4fba80
Pavlicheck <paanliff@gmail.com>
2017-10-14 22:14:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
-// Values
-var winWidth=0,
-	winHeight=0,
-	winHalf = 0;
 
-//resize
-	function resizeWin() {
-		winWidth = $(document).width();
-		winHeight = $(document).height();
-		if (winWidth > winHeight) {
-		$('.background').width(winWidth).height(winHeight);
-		$('.shatter-main').width(winWidth).height(winHeight);
-		$('.cockroach').width(winWidth*.15).height(winWidth*.15);
-	} else {
-		$('.background').width(winWidth).height(winHeight);
-		$('.shatter-main').width(winWidth).height(winHeight);
-		$('.cockroach').width(winWidth*.4).height(winWidth*.4);
-	}
-		// alert('works');
-		// winHalf = winWidth/2;
-	}
 
-$(window).resize(function() {
-		resizeWin();
-});
+darkOpacity(0, 'none')
 
-// Menu creater
-resizeWin();
+
+function darkOpacity(opacity, display) {
+$('.dark').css('display', 'block');
+$('.dark').animate({opacity: opacity}, 1000, function(){$(this).css('display', display)});
+}
+
+
+
+function scoreShow() {
+		darkOpacity(0.8, 'block');
+		$('.score').css('display','block').animate({left: 0}, 1000);
+}
+
+$('#score').click(scoreShow);
